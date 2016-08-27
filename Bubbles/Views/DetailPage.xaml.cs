7f31f014595598c856e981a1473ded9b664a88f1
Kaamraan <kaamraank@outlook.com>
2016-08-27 17:48:33 +1000

    Add project files.

---
+++ Bubbles/Views/DetailPage.xaml.cs
@@ -0,0 +1,13 @@
+using Bubbles.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace Bubbles.Views {
+    public sealed partial class DetailPage : Page {
+        public DetailPage() {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
